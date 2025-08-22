b45eee133ca8837bad9f9f77cd4bf74268e13a98
mingsyunchuang <mingsyun@hotmail.com>
2025-08-22 12:31:02 +0800

    Update rsi_monitor.py

--- rsi_monitor.py
+++ rsi_monitor.py
@@ -20,7 +20,7 @@ def get_rsi(symbol, period=14):
     avg_loss = loss.rolling(window=period, min_periods=period).mean()
     rs = avg_gain / avg_loss
     rsi = 100 - (100 / (1 + rs))
-    return round(rsi[-1], 2)
+    return round(rsi.iloc[-1], 2)
 
 def get_rsi_old(symbol, days=14):
     data = yf.download(symbol, period=f'{days+30}d', interval='1d')
